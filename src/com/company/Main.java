ae4522177492be022d052ba7cc8111e0687aab56
Fasttrackit2019 <vlad.lucian.dinu@gmail.com>
2019-12-19 20:42:12 +0200

    First

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package  com.company;
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        // write your code here
+        int x;
+        x = 5;
+        int sum=x+x;
+
+    }
+}
