3bf99f767d3b0c5c9b4a1c8e0db9da3ac72498a3
Fasttrackit2019 <vlad.lucian.dinu@gmail.com>
2020-01-06 22:08:56 +0200

    Assignment lab 9

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,11 +2,30 @@ package  com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello world");
-        // write your code here
-        int x;
-        x = 5;
-        int sum=x+x;
+
+        // Q1 assignment
+        System.out.println("Hello");
+        System.out.println("Vlad");
+
+        // Q2 assignment
+        int x = 2;
+        int y = 5;
+        int sum = x+y;
+        System.out.println(sum);
+
+        // Q3 assignment
+        float a = 100.5f;
+        float b = 17.5f;
+        float div = a/b;
+        System.out.println(div);
+
+        // Q4 assignment
+        System.out.println(-5 + 8 * 6);
+        System.out.println((55 + 9) % 9);
+        System.out.println(20 + (float) -3 * 5 / 8);
+        System.out.println(5 + 15 / 3 * 2 - 8 % 3);
+
+
 
     }
 }
