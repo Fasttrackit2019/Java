485ad23be53c2290304735a0d840b5f9355a5cc9
Fasttrackit2019 <vlad.lucian.dinu@gmail.com>
2020-01-23 20:28:23 +0200

    Lab4

---
+++ src/lab4/LogicalOp.java
@@ -0,0 +1,56 @@
+package lab4;
+
+public class LogicalOp {
+    public int verifyGreaterNumber(int first, int second) {
+        if (first > second) {
+            return first;
+        } else {
+            return second;
+        }
+    }
+
+    public String verifyText(String text) {
+        if (text.equals("FastTrack")) {
+            return ("Learning text comparison");
+        } else {
+            return ("Got to try some more");
+        }
+    }
+
+    public void donumber(int number) {
+        if (number >= 2 && number <= 8) {
+            System.out.println(number);
+        }
+    }
+
+    //Write a program called SumAndAverage to produce the sum of 1, 2, 3, ... , to 100
+//Also compute and display the average
+    public void printToHundred(int number) {
+        int sum = 0;
+        int count = 0;
+        for (int i = number; i <= 100; i++) {
+            count++;
+            sum = sum + i;
+            System.out.println("Numarul este " + i);
+        }
+        System.out.println("Media este " + sum / count);
+    }
+
+    public void printFromHundred(int first, int second) {
+        for (int i = first; i >= second; i--) {
+            System.out.println(i + ",");
+        }
+    }
+
+    //Sa se afiseze numerele pare de la 0 la -100
+    public void evenNumber(int number) {
+        for (int i = number; i >= -100; i--) {
+            if (i % 2 == 0) {
+                System.out.println(i);
+            }
+        }
+    }
+
+
+}
+
---
+++ src/lab4/Main.java
@@ -0,0 +1,28 @@
+package lab4;
+
+import lab4.LogicalOp;
+
+public class Main {
+//given the numbers see which one is the greatest an print it
+
+    public static void main(String[] args) {
+        int x = 3;
+        int y = 8;
+
+        LogicalOp op = new LogicalOp();
+        System.out.println(op.verifyGreaterNumber(x, y));
+
+//            Given a number, if it's equal to or higher than 2 and equal to or lower than 8, print the number
+        op.donumber(5);
+
+
+        //        Given a text input, if it is "FastTrack" then print "Learning text comparison" if not, print "Got to try some more"
+
+        String text = "FastTrack";
+        String result = op.verifyText(text);
+        System.out.println(result);
+        op.evenNumber(1);
+    }
+
+
+}
