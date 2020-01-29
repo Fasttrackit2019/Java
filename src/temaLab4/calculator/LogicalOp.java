fa931e9b238f9bb4ffde8d66b5b766caec6dadbd
Fasttrackit2019 <vlad.lucian.dinu@gmail.com>
2020-01-29 21:45:52 +0200

    Tema Lab4 Calculator - do while loops

--- src/temaLab4/calculator/LogicalOp.java
+++ src/temaLab4/calculator/LogicalOp.java
@@ -2,6 +2,8 @@ package temaLab4.calculator;
 
 public class LogicalOp {
 
+    //    FOR loops
+
     //    Q1 Assignment
     public void countToHundred(int number) {
         for (int i = number; i <= 100; i++) {
@@ -85,10 +87,150 @@ public class LogicalOp {
             }
             System.out.println("\n");
         }
-
     }
 
+    //    WHILE loops
+
+    //    Q1 Assignment
+    public void countToPositive(int i) {
+        do {
+            System.out.println(i);
+            i++;
+        } while (i <= 100);
+    }
+
+    //    Q2 Assignment
+    public void countToNegative(int i) {
+        do {
+            System.out.println(i);
+            i--;
+        } while (i >= -100);
+    }
+
+    //    Q3 Assignment
+    public void countBetweenVariables(int x, int y) {
+        do {
+            System.out.println(x);
+            x++;
+        } while (x <= y);
+    }
+
+    //    Q4 Assignment
+    public void countBetweenInterval(int x, int y) {
+        if (x < y) {
+            do {
+                System.out.println(x);
+                x++;
+            } while (x <= y);
+        } else if (x > y) {
+            do {
+                System.out.println(x);
+                x--;
+            } while (x >= y);
+        } else {
+            System.out.println("The numbers are identical");
+        }
+    }
+
+    //    Q5 Assignment
+    public void countEvenNumberToHundred(int x) {
+        do {
+            if (x % 2 == 0) {
+                System.out.println(x);
+            }
+            x++;
+        } while (x <= 100);
+    }
+
+    //    Q6 Assignment
+    public void countUnevenNumberToHundred(int x) {
+        do {
+            if (x % 2 != 0) {
+                System.out.println(x);
+            }
+            x++;
+        } while (x <= 100);
+    }
+
+    //    Q7 Assignment
+    public float getAverage(int first, int second) {
+        float sum = 0;
+        float average = 0;
+        int counter = 0;
+        while (first <= second) {
+            sum += first;
+            first++;
+            counter++;
+        }
+        average = sum / counter;
+        return average;
+    }
+
+    //    Q8 Assignment          ??????????????????????
+    public float getAverageDivSeven(int first, int second) {
+        float sum = 0;
+        float average = 0;
+        int counter = 0;
+        do {
+            if (sum % 7 == 0) {
+                average = sum / counter;
+                first++;
+                counter++;
+            }
+        } while (first < second);
+        return average;
+    }
+
+    //    Q9 Assignment
+    public void fibonacciTwenty(int number) {
+        int sum = 0;
+        do {
+            System.out.println(sum);
+            number++;
+            sum = sum + number;
+        } while (number <= 20);
+    }
+
+    //    Q10 Assignment                       ??????????????????????????
+    public void cozaLozaWoza(int i) {
+
+        do {
+            System.out.println(i);
+            if (i % 3 == 0) {
+                System.out.print("Coza");
+            }
+            if (i % 5 == 0) {
+                System.out.print("Loza");
+            }
+            if (i % 7 == 0) {
+                System.out.print("Woza");
+            }
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println("CozaLoza");
+            }
+            if (i % 5 == 0 && i % 7 == 0) {
+                System.out.println("WozaLoza");
+            }
+            if (i % 3 == 0 && i % 5 == 0 && i % 7 == 0) {
+                System.out.println("CozaLozaWoza");
+            }
+            i++;
+        } while (i <= 110);
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
--- src/temaLab4/calculator/Main.java
+++ src/temaLab4/calculator/Main.java
@@ -6,37 +6,81 @@ public class Main {
 
         LogicalOp op = new LogicalOp();
 
+//        FOR loops
+
 //        Q1 Assignment
-        op.countToHundred(1);
+//        op.countToHundred(1);
 
 //        Q2 Assignment
-        op.countToNumber(5);
+//        op.countToNumber(5);
 
 //        Q3 Assignment
-        op.countBetweenNumbers(5, 50);
+//        op.countBetweenNumbers(5, 50);
 
 
 //        Q4 Assignment
-        op.countFromGivenNumbers(1, 10);
+//        op.countFromGivenNumbers(1, 10);
 
 //        Q5 Assignment
-        op.evenNumber(1);
+//        op.evenNumber(1);
 
 //        Q6 Assignment
-        op.unevenNumber(1);
+//        op.unevenNumber(1);
 
 //        Q7 Assignment
-        op.sumFromOneToHundred(1);
+//        op.sumFromOneToHundred(1);
 
 //        Q8 Assignment
-        op.printToHundred(1);
+//        op.printToHundred(1);
 
 //        Q9 Assignment
-        op.printPattern(1, 7);
+//        op.printPattern(1, 7);
+
+
+//    DO - WHILE loops
+
+//    Q1 Assignment
+//        op.countToPositive(1);
+
+//        Q2 Assignment
+//        op.countToNegative(0);
+
+//        Q3 Assignment
+//        op.countBetweenVariables(1,10);
+
+//        Q4 Assignment
+//        op.countBetweenInterval(1,20);
+
+//        Q5 Assignment
+//        op.countEvenNumberToHundred(1);
+
+//        Q6 Assignment
+//        op.countUnevenNumberToHundred(1);
+
+//        Q7 Assignment
+//        System.out.println(op.getAverage(111,8899));
+
+//        Q8 Assignment
+//        System.out.println(op.getAverageDivSeven(1,21));       ?????????????????
+
+//        Q9 Assignment
+//        op.fibonacciTwenty(0);
+
+//        Q10 Assignment
+//        op.cozaLozaWoza(1);                 ????????????????
+
+
+
+
+
+
+
+
+
+
+
 
     }
-
-
 }
 
 
